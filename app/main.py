bd565ee5713f7b32ee83f3fa0e4c3f9d5129e55e
Mohammed Omair Mohiuddin <mohammedomair07@gmail.com>
2024-11-17 15:36:24 -0600

    Added 'git cat-file' functionality

--- app/main.py
+++ app/main.py
@@ -1,12 +1,10 @@
 import sys
 import os
-
+import zlib
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!", file=sys.stderr)
 
-    
     command = sys.argv[1]
     if command == "init":
         os.mkdir(".git")
@@ -16,7 +14,18 @@ def main():
             f.write("ref: refs/heads/main\n")
         print("Initialized git directory")
     elif command == "cat-file":
-        print("pass")
+        blob_sha = sys.argv[-1]
+        dir = blob_sha[0:2]
+        file = blob_sha[2:]
+        file = file.strip("\r")
+        file_path = f".git/objects/{dir}/{file}"
+        with open(file_path, 'rb') as f_in:
+            compressed_data = f_in.read()
+        decompressed_data = zlib.decompress(compressed_data)
+        size = decompressed_data.split(b'\x00', 1)[0]
+        data = decompressed_data.split(b'\x00', 1)[1]
+        print(data.decode("utf-8"))
+
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
